763ff5ed101b77428ceb5b3bfc98d1f9a68839ce
DiyanTonchev <Diyantonchev@gmail.com>
2015-05-14 15:56:47 +0300

    Added new homework

---
+++ Homeworks/C#/StringAndTextProcessing/StringLength/StringLength.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class StringLength
+{
+    static void Main()
+    {
+        
+        string input = Console.ReadLine();
+        var output = new StringBuilder(20,20);
+
+        if (input.Length > 20)
+        {
+           output.Append(input.Remove(20, input.Length - 20));
+        }
+        else if (input.Length < 20)
+        {
+            output.Append(input);
+            output.Append(new string('*', 20 - input.Length));
+        }
+        Console.WriteLine(output);
+    }
+}
