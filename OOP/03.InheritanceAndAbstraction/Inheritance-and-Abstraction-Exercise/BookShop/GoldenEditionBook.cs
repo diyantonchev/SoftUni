11db199bbe54aaf9d58c8de0f3913fd44ae0ab79
Diyan Tonchev <diyantonchev@gmail.com>
2015-06-27 17:35:30 +0300

    Some corrections

---
+++ OOP/03.InheritanceAndAbstraction/Inheritance-and-Abstraction-Exercise/BookShop/GoldenEditionBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class GoldenEditionBook : Book
+{
+    public GoldenEditionBook(string title, string author, double price) : base(title, author, price) 
+    {
+        this.Price = price;
+    }
+
+    public override double Price
+    {
+        get
+        {
+            return (base.Price * (30/100) + base.Price);
+        }
+        protected set
+        {
+            base.Price = value;
+        }
+    }
+}
